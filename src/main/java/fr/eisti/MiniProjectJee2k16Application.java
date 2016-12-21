b26b9c1848676eab83971d248c8d3c70ec3ed8d9
croatian91 <ivan.hanzek.hr@gmail.com>
2016-12-21 17:43:01 +0100

    Maven project imported

---
+++ src/main/java/fr/eisti/MiniProjectJee2k16Application.java
@@ -0,0 +1,12 @@
+package fr.eisti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectJee2k16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectJee2k16Application.class, args);
+	}
+}
