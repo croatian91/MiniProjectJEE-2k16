fe09b87d219867cddb8b652c95c446d9f730b292
croatian91 <ivan.hanzek.hr@gmail.com>
2016-12-21 23:07:46 +0100

    Init test database

---
+++ src/test/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO `Station` (
+  `id`,
+  `number`,
+  `name`,
+  `address`,
+  `location`
+)
+VALUES
+  (1,
+   31705,
+   '31705 - CHAMPEAUX (BAGNOLET)',
+   'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+   'POINT(48.8645278209514 2.416170724425901)'
+  );
+
+INSERT INTO `Station` (
+  `id`,
+  `number`,
+  `name`,
+  `address`,
+  `location`
+)
+VALUES
+  (2,
+   10042,
+   '10042 - POISSONNIÈRE - ENGHIEN',
+   '52 RUE D''ENGHIEN / ANGLE RUE DU FAUBOURG POISSONIERE - 75010 PARIS',
+   'POINT(48.87242006305313 2.348395236282807)'
+  );
