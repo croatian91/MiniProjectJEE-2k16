29b98f344374a6a89e0b0d56059efc5f9bfaf91b
croatian91 <ivan.hanzek.hr@gmail.com>
2016-12-21 23:05:56 +0100

    Created Station repository

---
+++ src/main/java/fr/eisti/repositories/StationRepository.java
@@ -0,0 +1,12 @@
+package fr.eisti.repositories;
+
+import fr.eisti.domain.Station;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by ivan on 21/12/16.
+ */
+@Repository
+public interface StationRepository extends CrudRepository<Station, Integer> {
+}
