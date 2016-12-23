b0aa9e356676f397455d5240d9f3859889f7699d
croatian91 <ivan.hanzek.hr@gmail.com>
2016-12-23 15:39:25 +0100

    Client can now retrieve information about a station

--- src/main/java/fr/eisti/web/StationController.java
+++ src/main/java/fr/eisti/web/StationController.java
@@ -1,10 +1,16 @@
 package fr.eisti.web;
 
+import fr.eisti.domain.Station;
+import fr.eisti.domain.StationRealTime;
 import fr.eisti.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 /**
  * Created by ivan on 21/12/16.
@@ -28,5 +34,11 @@ public class StationController {
     @RequestMapping("/stations/district/{district}")
     public Iterable ListByDistrict(@PathVariable("district") int district) {
         return stationRepository.findByDistrict(district);
+    }
+
+    @RequestMapping("/stations/station/{station_number}")
+    public StationRealTime getInformation(@PathVariable("station_number") int stationNumber) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://api.jcdecaux.com/vls/v1/stations/" + stationNumber + "?contract=Paris&apiKey=85871bc0de9ac4eef048c1d768fe08909b3432d7", StationRealTime.class);
     }
 }
